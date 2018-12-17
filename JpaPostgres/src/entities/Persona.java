72ac41236ce97df6fcc47d391cf8934a0ee280b2
Luis Granada L <2220161033@estudiantesunibague.edu.co>
2018-12-17 11:36:48 -0500

    metodos get y constructor customizado

--- JpaPostgres/src/entities/Persona.java
+++ JpaPostgres/src/entities/Persona.java
@@ -33,6 +33,42 @@ public class Persona implements Serializable {
 		
 	}
 
-	
+	public Persona(long identificion, String primerNombre, String segundoNombre, String primerApellido,
+			String segundoApellido, int codigoMunicipio, int codigoDepartamento) 
+	{
+		this.identificion = identificion;
+		this.primerNombre = primerNombre;
+		this.segundoNombre = segundoNombre;
+		this.primerApellido = primerApellido;
+		this.segundoApellido = segundoApellido;
+		Departamento departamento= new Departamento(codigoDepartamento, null);
+		Municipio m=new Municipio(codigoMunicipio, null, departamento);
+		this.municipioNacimiento=m;
+	}
+
+	//metodos set
+	public long getIdentificion() {
+		return identificion;
+	}
+
+	public String getPrimerNombre() {
+		return primerNombre;
+	}
+
+	public String getSegundoNombre() {
+		return segundoNombre;
+	}
+
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+
+	public Municipio getMunicipioNacimiento() {
+		return municipioNacimiento;
+	}
 
 }
